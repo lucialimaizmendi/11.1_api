4021b02755ce80e9bcef5cdd887d3bd2a10bfb28
LuciaLimaIzmendi <limaizmendi@gmail.com>
2023-09-11 19:42:37 -0300

    Listo

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 let video = document.getElementsByTagName("video")
 
-fetch("https://owen-wilson-wow-api.onrender.com/wows/random")
+newVideo();
+
+function playVideo() {
+  video[0].play();
+}
+
+function newVideo() {
+  fetch("https://owen-wilson-wow-api.onrender.com/wows/random")
   .then(function(response) {
     if (!response.ok) {
       throw new Error('Error en la solicitud HTTP: ' + response.status);
@@ -8,16 +15,10 @@ fetch("https://owen-wilson-wow-api.onrender.com/wows/random")
     return response.text();
   })
   .then(function(data) {
-    console.log(JSON.parse(data));
     video[0].src = JSON.parse(data)[0].video['480p'];
     video[0].load();
   })
   .catch(function(error) {
     console.error('Error: ' + error);
   });
-
-var playButton = document.getElementById('play');
-
-playButton.addEventListener('click', function() {
-  video.play();
-});
+} 
